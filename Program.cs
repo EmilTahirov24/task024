833f54c38d570c7c51343e24f2f7a71f867120ce
EmilTahirov24 <emil.tahirov24@gmail.com>
2026-03-04 18:58:57 +0400

    task024

---
+++ Program.cs
@@ -0,0 +1,215 @@
+﻿//for(int=1;int<10; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+//int cem = 0;
+
+//for (int i = 1; i <= 50; i++)
+//{
+//    cem += i;
+//}
+
+//Console.WriteLine(cem);
+
+
+//for (int i = 10; i >= 1; i--)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+//for (int i = 1; i <= 20; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+
+
+//int cem = 0;
+//int say = 0;
+//for (int i = 1; i < 100; i++)
+//{
+//    cem += i;
+//    say += i;
+//    int orta = cem / say;
+//    Console.WriteLine(orta);
+
+//}
+
+
+//for (int i = 1; i < 100; i++)
+//{
+//    if (i % 5 == 0 && i % 7 == 0) ;
+//    Console.WriteLine(i);
+//}
+//;
+
+
+//int cemcut = 0;
+//int cemtek = 0;
+//for (int i = 1; i < 10; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        cemcut += i;
+//    }
+//    else
+//    {
+//        cemtek += i;
+//    }
+//}
+//Console.WriteLine($"{cemtek}, {cemcut}");
+
+using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
+//int point = 95;
+//if (point >= 90)
+//    Console.WriteLine("A");
+//else if (point >= 80)
+//    Console.WriteLine("B");
+//else if (point >= 70)
+//    Console.WriteLine("C");
+//else if (point >= 60)
+//    Console.WriteLine("D");
+//else if (point >= 50)
+//    Console.WriteLine("E");
+//else
+//    Console.WriteLine("F");
+
+
+//int[] arr = [12, 14, 35, 56, 68, 93];
+//int cem = 0;
+//for (int i=0; i<arr.Length; i++)
+//{
+//    cem += arr[i];
+//}
+//Console.WriteLine(cem);
+
+
+
+
+//int[] arr = [12, 14, 35, 56, 68, 93];
+//int cem = 0;
+//for (int i =0; i<arr.Length; i++)
+//{
+//    if (i % 2 == 1)
+//        Console.WriteLine(arr[i]);
+//}
+
+
+
+//int[] arr = [12, 14, 35, 56, 68, 93];
+//int cem2 = 0;
+//for(int i= 0; i<arr.Length, i++)
+//{
+//    if (i % 2 == 0)
+//        cem2 += arr[i];
+//}
+//Console.WriteLine(cem2);
+
+
+
+
+//Console.Write("yasi yaz");
+//int yas = int.Parse(Console.ReadLine());
+
+//if (yas > 0 && yas < 125)
+//{
+//    if (yas >= 65)
+//        Console.WriteLine("pensiya yasina catmamisan");
+//    else
+//        Console.WriteLine("pensiya yasina catmamisan");
+//}
+//else
+//{
+//    Console.WriteLine("musbet ve 125den kicik olmalidir");
+//}
+
+
+
+//Console.WriteLine("eded daxil et");
+//int eded = int.Parse(Console.ReadLine());
+
+//if (eded > 0)
+//{
+//    if (eded % 2 == 0)
+//        Console.WriteLine("eded cutdur");
+//    else
+//        Console.WriteLine("eded tekdir");
+//}
+//else
+//{
+//    Console.WriteLine("Musbet eded daxil edin");
+//}
+
+
+//Console.Write("Musbet eded daxil et");
+//int eded = int.Parse(Console.ReadLine());
+
+//if (eded > 0)
+//    Console.WriteLine(eded % 3 == 0 && eded % 5 == 0 ? "3 və 5e bölünür" : "bolunmur");
+//else
+//    Console.WriteLine("musbet eded daxil edin");
+
+//Console.Write("Musbet eded daxil et");
+//int eded = int.Parse(Console.ReadLine());
+//if (eded % 2 == 0)
+//{
+//    Console.WriteLine("bolundu");
+
+//}
+//else int qaliq = eded %2 {
+
+
+//Console.WriteLine(qaliq);
+//}
+
+
+//Console.Write("birinci eded daxil edin ");
+//int a = int.Parse(Console.ReadLine());
+
+//Console.Write("ikinci ededi daxil edin");
+//int b = int.Parse(Console.ReadLine());
+
+//int hasil = a * b;
+
+//if (hasil > 0)
+//    Console.WriteLine("hasil musbet ededir");
+//else if (hasil < 0)
+//    Console.WriteLine("hasil menfidir");
+//else
+//    Console.WriteLine("Hasil sifirdir ");
+
+
+//string name = Console.ReadLine();
+//string surname = Console.ReadLine();
+//string fullName = name + "" + surname;
+//Console.WriteLine(fullName);
+
+
+//int ceki = int.Parse(Console.ReadLine());
+//int boy = int.Parse(Console.ReadLine());
+//int bmi = ceki / (boy * boy);
+//if (bmi >= 30)
+//    Console.WriteLine("kok");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
