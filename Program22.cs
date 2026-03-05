adebbd82d68637ce1aa47f5210d1bb2b3b107fb6
EmilTahirov24 <emil.tahirov24@gmail.com>
2026-03-05 18:48:43 +0400

    task25

---
+++ Program22.cs
@@ -0,0 +1,218 @@
+﻿//string sentence = Console.ReadLine();
+//int uzunluq = 0;
+//for(int i=0;i<sentence.Length; i++)
+//{
+//    uzunluq += 1;
+//}
+//Console.WriteLine(uzunluq);
+
+//static void Main()
+//{
+//    Console.WriteLine("Enter sentence :");
+//    string setence = Console.ReadLine();
+
+//    int count = CharecterCount(sentence);
+//    Console.WriteLine(count);
+//}
+//static int CharecterCount(string text)
+//{
+//    return text.Length;
+//}
+
+
+//int ay = 10;
+//switch (ay)
+//{
+//    case 1:
+//        Console.WriteLine("yanvar");
+//        break;
+//    case 2:
+//        Console.WriteLine("fevral");
+//        break;
+//    case 3:
+//        Console.WriteLine("mart");
+//        break;
+//    case 4:
+//        Console.WriteLine("aprel");
+//        break;
+//    case 5:
+//        Console.WriteLine("may");
+//        break;
+//    case 6:
+//        Console.WriteLine("iyun");
+//        break;
+//    case 7:
+//        Console.WriteLine("iyul");
+//        break;
+//    case 8:
+//        Console.WriteLine("aqust");
+//        break;
+//    case 9:
+//        Console.WriteLine("sentyabr");
+//        break;
+//    case 10:
+//        Console.WriteLine("oktyabr");
+//        break;
+//    case 11:
+//        Console.WriteLine("noyabr");
+//        break;
+//    case 12:
+//        Console.WriteLine("dekabr");
+//        break;
+//    default:
+//        Console.WriteLine("yanlisdir");
+//        break;
+
+//}
+
+
+//int enboyuk(int[] arr)
+//{
+//    int max = arr[0];
+//    for (int i=0; i<arr.Length; i++)
+//    {
+//        if (arr[i] > max)
+//        {
+//            max = arr[i];
+//        }
+//    }
+//    return max;
+//}
+
+
+//double CalculateAvarage(int a, int b, int c)
+//{
+//    double avarage = (a + b + c) / 3;
+//    return avarage;
+//}
+//int a = 7;
+//int b = 8;
+//int c = 9;
+//double result = CalculateAvarage(a, b, c);
+//Console.WriteLine(result);
+
+
+
+//gg
+//int[] eded ={4, 6, 8, 10 };
+//Console.WriteLine("eded daxil et");
+//int a = int.Parse(Console.ReadLine());
+
+//static bool check (int eded, int arr)
+//{
+//    for(int i=0; i < arr.Length; i++)
+//    {
+//        arr[i] = eded;
+//        return true;
+
+//    }
+//    return false;
+//}
+
+
+
+//bool yoxla(int[] arr, int eded)
+//{
+//    for (int i = 0; i < arr.Length; i++)
+//    {
+//        if (arr[i] == eded)
+//        {
+//            return true;
+//        }
+//    }
+
+//    return false;
+//}
+
+
+
+
+//int EnKicik(int[] arr)
+//{
+//    int min = arr[0];
+
+//    for (int i = 0; i < arr.Length; i++)
+//    {
+//        if (arr[i] < min)
+//        {
+//            min = arr[i];
+//        }
+//    }
+
+//    return min;
+//}
+//int[] array = { 33, 5, 8, 10, 1, 3, 15, 20, 25 };
+//int min = array[0];
+//for (int i = 0; i < array.Length; i++)
+//{
+//    if (array[i] < min)
+//    {
+//        min = array[i]; 
+//    }
+//}
+
+//Console.WriteLine("kicik element: " + min);
+
+
+
+//int[] array = { 33, 5, 8, 10, 1, 3, 15, 20, 25 };
+//int max = array[0];
+//for (int i = 0; i < array.Length; i++)
+//{
+//    if (array[i] < max)
+//    {
+//        max = array[i];  
+//    }
+//}
+
+//Console.WriteLine("kicik element: " + max);
+
+
+
+
+//int[] array = { 33, 5, 8, 10, 1, 3, 15, 20, 25 };
+// for (int i=0; i < array.Length; i++)
+//{
+//    if (array[i] % 2 == 0)
+//    {
+//        Console.WriteLine(array[i]);
+//     else
+//            continue
+//    }
+//}
+
+
+//int[] array = { 33, 5, 8, 10, 1, 3, 15, 20, 25 };
+//int sum = 0;
+//for (int i = 0; i <array.Length; i++)
+//{
+//    if (array[i] % 2 == 1)
+//    {
+//        sum += array[1];
+//    }
+//}
+//Console.WriteLine("tek ededlerin cemi:" + sum);
+
+//int[] array = { 33, 5, 8, 10, 1, 3, 15, 20, 25 };
+// for(int i=0; i<array.Length; i++)
+//{
+//    if (array[i] % 2 == 0)
+//    {
+//        Console.WriteLine(array[i]);
+//    }
+
+//}
+
+
+
+int[] array = { 33, 5, 8, 10, 1, 3, 15, 20, 25 };
+int hasil = 1;
+
+
+
+
+
+
+
+
+
